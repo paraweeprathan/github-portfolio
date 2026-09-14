fcd09f44a17ddc7a92c43df8b87d73c829e8eee8
paraweeprathan <parawee.prathan@gmail.com>
2026-09-14 13:14:28 +0700

    Add theme toggle functionality for light mode

--- script.js
+++ script.js
@@ -147,3 +147,14 @@ if (typingElement) {
 
     typeEffect();
 }
+const themeToggle = document.getElementById("theme-toggle");
+
+themeToggle.addEventListener("click", () => {
+    document.body.classList.toggle("light-mode");
+
+    if (document.body.classList.contains("light-mode")) {
+        themeToggle.textContent = "☾";
+    } else {
+        themeToggle.textContent = "☀";
+    }
+});
